754d46f84b60112a159faf9623974bf52076f704
Eric Gratta <eric.gratta@gmail.com>
2022-05-10 22:49:35 -0400

    Create example due date form with Mantine

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { NextPage } from 'next';
+import { DatePicker } from '@mantine/dates';
+import { Button, Card, Container, Group, Stack, Text } from '@mantine/core';
 
 const Home: NextPage = () => {
-  return <div>Meow</div>;
+  const [date, setDate] = useState<Date>(new Date());
+
+  const handleDateChange = useCallback((newDate: Date | null) => {
+    if(newDate) {
+      setDate(newDate);
+    }
+  }, []);
+
+  const handleClick = useCallback(() => {
+    alert('Save!');
+  }, []);
+
+  return (
+    <Container>
+      <Card shadow="sm">
+        <Stack>
+          <Group>
+            <Text>Due Date</Text>
+            <DatePicker
+              clearable={false}
+              onChange={handleDateChange}
+              placeholder="Pick dates range"
+              value={date}
+            />
+          </Group>
+          <Button
+            onClick={handleClick}
+          >
+            Submit
+          </Button>
+        </Stack>
+      </Card>
+    </Container>
+  );
 };
 
 export default Home;
