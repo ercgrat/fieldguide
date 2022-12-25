2ba7136a96f0494116bf5b5c80d70bb8b8e365d1
Eric Gratta <eric.gratta@gmail.com>
2022-12-25 18:22:46 -0500

    Remove supabase references

--- utils/const.ts
+++ utils/const.ts
@@ -1,3 +1,1 @@
-export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
-export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? '';
 export const IS_PROD = process.env.NODE_ENV === 'production';
